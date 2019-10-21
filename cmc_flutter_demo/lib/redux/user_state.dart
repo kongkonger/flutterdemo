e58600406fc1ffbaf7ab27b9631a0a3770610e49
kongkonger <ming159357456@163.com>
2019-10-22 00:35:30 +0800

    router storebuilder
    Signed-off-by: kongkonger <ming159357456@163.com>

--- cmc_flutter_demo/lib/redux/user_state.dart
+++ cmc_flutter_demo/lib/redux/user_state.dart
@@ -12,9 +12,17 @@ UserState appReducer(UserState state, action) {
 
   );
 }
+//cmc add 正确的顺序是  1、State结构  ------
+//                     2、 appReducer 接收state结构和action---------并对应state的变量名称设置 reducer对象
+//                     3、具体的reducer对象连接 reducer和action
+//cmc add -----------  4、具体的action实现类 通过接收action值改变state值  ---------
+//                     5、当然先要有 action类
 
 /// redux 的 combineReducers, 通过 TypedReducer 将 UpdateUserAction 与 reducers 关联起来
 final UserReducer = combineReducers<String>([
+  //cmc add _updateLoaded 相当于预先设置的回调，
+  // cmc add当调用 store.dispatch(new ***action(state值))时候，则会回调到此 _updateLoaded回调中
+  //cnc add 然后就可以利用 action中的值来 更新 state中的值了
   TypedReducer<String, UpdateUserAction>(_updateLoaded),
 ]);
 
